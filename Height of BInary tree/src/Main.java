37f69b4736a55313edaebea0ddc5964b0d78515e
Himanshu <ydvhimanshu461@gmail.com>
2025-09-02 14:28:02 +0530

    Height of binary tree

---
+++ Height of BInary tree/src/Main.java
@@ -0,0 +1,37 @@
+class Main {
+    // Make Node a static nested class so it can be accessed from main
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // Make BinaryTreeHeight a static nested class or merge method to Main
+    public static class BinaryTreeHeight {
+        public static int height(Node root) {
+            if (root == null) {
+                return 0;
+            }
+            int leftHeight = height(root.left);
+            int rightHeight = height(root.right);
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        // Call the static method using the class name
+        System.out.println("Height of tree: " + BinaryTreeHeight.height(root));
+    }
+}
