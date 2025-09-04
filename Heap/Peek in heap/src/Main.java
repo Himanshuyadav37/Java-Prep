b0ee306540c6d88495ec118fe40606796fff8a8b
Himanshu <167640226+Himanshuyadav37@users.noreply.github.com>
2025-09-04 13:37:49 +0530

    Add files via upload
    peek in heap

---
+++ Heap/Peek in heap/src/Main.java
@@ -0,0 +1,53 @@
+
+import java.util.*;
+
+public class Main {
+
+    static class Heap {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        // Add element to Min Heap
+        public void add(int data) {
+            arr.add(data); // Step 1: always add at last index
+
+            int child = arr.size() - 1; // child index
+            int par = (child - 1) / 2;  // parent index
+
+            // Step 2: Bubble up while heap property is violated
+            while (child > 0 && arr.get(child) < arr.get(par)) {
+                // Swap
+                int temp = arr.get(child);
+                arr.set(child, arr.get(par));
+                arr.set(par, temp);
+
+                // Move up
+                child = par;
+                par = (child - 1) / 2;
+            }
+        }
+
+        // Print heap
+        public void printHeap() {
+            System.out.println(arr);
+        }
+
+        public void peek() {
+            System.out.println(arr.get(0));
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        Heap heap = new Heap();
+        heap.add(5);
+        heap.add(3);
+        heap.add(8);
+        heap.add(1);
+        heap.add(4);
+
+        System.out.println("Min Heap: ");
+        heap.printHeap(); // Output will be a valid Min Heap
+        heap.peek();
+    }
+}
