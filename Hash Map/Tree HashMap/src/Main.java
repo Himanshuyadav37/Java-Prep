f4217f0e836a58711e1d15c43b943512dde9b61b
Himanshu <167640226+Himanshuyadav37@users.noreply.github.com>
2025-09-08 12:21:21 +0530

    Add files via upload
    Tree HashMap

---
+++ Hash Map/Tree HashMap/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        TreeMap<String, Integer> map = new TreeMap<>();
+        // insertion  - O(1)         Hashmap are unordered
+        map.put("India",1);
+        map.put("China",2);
+        map.put("Russia",3);
+
+        // print
+        System.out.println("Insertion in Hashmap : " + map);
+        System.out.println("------------------------------------------------------------");
+
+
+        // get  - O(1)
+        int Ranking = map.get("India");
+        System.out.println("Get in Hashmap : " + Ranking);
+        System.out.println("-----------------------------------------------------------");
+
+
+        //containsKey  - O(1)
+        System.out.print("ContainsKey in Hashmap : " + map.containsKey("India") + "        ");     // true
+        System.out.println("ContainsKey in Hashmap : " + map.containsKey("America"));   // False
+        System.out.println("-----------------------------------------------------------");
+
+
+        //remove - O(1)
+        System.out.println("Remove in HashMap :  "  + map.remove("China"));
+        System.out.println("After removing China " + map);
+        System.out.println("-----------------------------------------------------------");
+
+
+        // size
+        System.out.println("Size of Hashmap : " + map.size());
+        System.out.println("-----------------------------------------------------------");
+
+
+        // Is Empty
+        System.out.println("Is Empty : " + map.isEmpty());
+
+
+        // clear
+        map.clear();
+    }
+    }
